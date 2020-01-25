51e04588f0066c173d03b12985586ccd84448b70
Rory Hayes <rorosaurus@gmail.com>
2020-01-24 20:18:46 -0800

    init first test examples

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # NGC-6946
  The Fireworks Galaxy
+https://en.wikipedia.org/wiki/NGC_6946
---
+++ SimpleGifExample/SimpleGifExample.ino
@@ -0,0 +1,27 @@
+#define BASICSPIFFS
+// Use NeoMatrix API, even if it may use the SmartMatrix backend depending on the CPU
+#define NEOMATRIX
+#define DISABLE_MATRIX_TEST
+#include "GifAnim_Impl.h"
+
+
+// If the matrix is a different size than the GIFs, allow panning through the GIF
+// while displaying it, or bouncing it around if it's smaller than the display
+int OFFSETX = 0;
+int OFFSETY = 0;
+int FACTX = 0;
+int FACTY = 0;
+
+// this is the location of the gif you want to loop
+const char *pathname = "/gifs/236_spintriangle.gif";
+
+// Setup method runs once, when the sketch starts
+void setup() {
+    sav_setup();
+    if (sav_newgif(pathname)) delay(100000); // while 1 loop only triggers watchdog on ESP chips
+}
+
+// loop method continuously repeats after setup() is run
+void loop() {
+    sav_loop();
+}
